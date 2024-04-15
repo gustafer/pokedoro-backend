d029ee41c48fa55e0794e657b5de8f3727c5e3fc
ustav <gustavobreasy@gmail.com>
2024-04-15 20:31:47 -0300

    add check for user id with jwt when adding pokemon to user

--- src/routes/user/add-pokemon-to-user.ts
+++ src/routes/user/add-pokemon-to-user.ts
@@ -2,11 +2,14 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { prisma } from "../../lib/prisma";
 import { z } from "zod";
+import { auth } from "../../middlewares/auth";
+import { checkUserId } from "../../utils/auth/check-user-id";
 
 export async function addPokemonToUser(app: FastifyInstance) {
     app
         .withTypeProvider<ZodTypeProvider>()
         .put('/user/pokemon', {
+            preValidation: auth,
             schema: {
                 body: z.object({
                     userId: z.string().cuid(),
@@ -32,16 +35,21 @@ export async function addPokemonToUser(app: FastifyInstance) {
                     id: userId
                 },
                 select: {
+                    id: true,
                     pokemons: true,
                 }
             })
 
+
             if (!existingUser) return res.status(404).send("user not found")
+
+            checkUserId(req, res, existingUser.id)
+
 
             // check if user already has pokemons, this code is a piece of shit
             for (let index = 0; index < existingUser.pokemons.length; index++) {
                 if (pokemonId == existingUser.pokemons[index].id) {
-                    return res.status(404).send({ message: "user already has pokemon!"})
+                    return res.status(404).send({ message: "user already has pokemon!" })
                 }
             }
 
@@ -63,7 +71,7 @@ export async function addPokemonToUser(app: FastifyInstance) {
                 where: {
                     id: userId
                 },
-                select:{
+                select: {
                     id: true,
                     pokemons: true,
                 }
