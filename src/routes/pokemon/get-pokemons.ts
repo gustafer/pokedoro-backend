bf3e0849197da87e7a14c34a75e1bf795fb25856
ustav <gustavobreasy@gmail.com>
2024-04-18 18:31:36 -0300

    idk what is happening

--- src/routes/pokemon/get-pokemons.ts
+++ src/routes/pokemon/get-pokemons.ts
@@ -31,7 +31,6 @@ export async function getPokemons(app: FastifyInstance) {
                         mode: 'insensitive'
                     }
                 } : {
-
                 }
             })
 
